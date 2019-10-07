b42ee3df4905d08feb63e5f96818df002e73d526
Lisa Jo <lisaxjo@gmail.com>
2019-10-07 17:03:40 -0400

    feat: connecting to database issue

--- Assets/Registration.cs
+++ Assets/Registration.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
 public class Registration : MonoBehaviour
@@ -18,21 +19,35 @@ public class Registration : MonoBehaviour
         WWWForm form = new WWWForm();
         form.AddField("name", nameField.text);
         form.AddField("password", passwordField.text);
-        WWW www = new WWW("http://localhost/sqlconnect/register.php", form);
-        yield return www;
-        if(www.text == "0")
+        string url = "http://localhost/sqlconnect/register.php";
+     
+        Debug.Log("this is name" + nameField.text);
+        Debug.Log("this is password " + passwordField.text);
+
+        var postRequest = UnityWebRequest.Post(url, form);
+        Debug.Log(postRequest);
+        //UnityWebRequest www = UnityWebRequest.Post(url, form);
+        yield return postRequest.SendWebRequest();
+
+        //WWW www = new WWW(url, form);
+        //    yield return www;
+        //if(www.text == "0")
+        //if (string.IsNullOrEmpty(www.error))
+
+        //if (!www.isNetworkError || !www.isHttpError)
+        if(!postRequest.isNetworkError || !postRequest.isHttpError)
         {
-            Debug.Log("user created successfully");
+            //Debug.Log("user created successfully" + www);
             UnityEngine.SceneManagement.SceneManager.LoadScene(0);
         }
         else
         {
-            Debug.Log("this was not successful. Error" + www.text);
+            //Debug.Log("this was not successful. Error" + www);
         }
     }
     public void VerifyInputs()
     {
-        submitButton.interactable = (nameField.text.Length >= 8 && passwordField.text.Length >= 8);
+        submitButton.interactable = (nameField.text.Length >= 4 && passwordField.text.Length >= 4);
 
     }
     // Start is called before the first frame update
