92306e587e6f6842cf83f6c74ed307d733735704
Kat Le <kl2853@barnard.edu>
2019-10-08 15:01:29 -0400

    feat: knight NPC appearing over single image target

---
+++ Assets/Scripts/NPCTalk.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCTalk : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Stationary)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+                if (Physics.Raycast(ray, out _))
+                {
+                    Debug.Log("hewwo");
+                }
+            }
+        }
+    }
+}
