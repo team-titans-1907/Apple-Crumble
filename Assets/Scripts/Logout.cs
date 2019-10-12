618995764eb4de64927ad4a8bfd89be10bf77d93
Lisa Jo <lisaxjo@gmail.com>
2019-10-12 19:34:38 -0400

    feat: user can logout

---
+++ Assets/Scripts/Logout.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Logout : MonoBehaviour
+{
+  public void hasQuit()
+    {
+        Debug.Log("you have quit the game!");
+        Application.Quit();
+        SceneManager.LoadScene(0);
+    }
+}
