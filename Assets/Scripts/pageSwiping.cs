b6a6d5e7f0e5e00727b2093bf893d480e2723fd4
Shirin Setayesh <shannonsetayesh@gmail.com>
2019-10-14 17:20:44 -0400

    refactor: incrementing and decrementing scene

--- Assets/Scripts/pageSwiping.cs
+++ Assets/Scripts/pageSwiping.cs
@@ -8,7 +8,6 @@ public class PageSwiping : MonoBehaviour
     public Vector3 userSwipe;
     private int currentSceneIndex = 0;
     private int totalSceneCount = 0;
-    //public Animation animation;
     private static int counter = 0;
 
     //minimum length of swipe for the swipe to register
@@ -18,14 +17,12 @@ public class PageSwiping : MonoBehaviour
     {
         currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         totalSceneCount = SceneManager.sceneCountInBuildSettings;
-        Debug.Log("totalSceneCount --> " + totalSceneCount);
     }
 
 
     void Update()
     {
         counter++;
-        Debug.Log("THE CURRENT SCENE IS AT INDEX: " + currentSceneIndex);
 
         /*for swiping*/
 
@@ -46,15 +43,16 @@ public class PageSwiping : MonoBehaviour
                 {
                     if (isSwipeRight())
                     {
-                        Debug.Log("SCENE CHANGE RIGHT");
-                        updateCurrentSceneIndex(true);
+                        //updateCurrentSceneIndex(true);
+                        SceneManager.LoadScene(currentSceneIndex + 1);
                     }
                     else
                     {
-                        Debug.Log("SCENE CHANGE LEFT");
-                        updateCurrentSceneIndex(false);
+                        SceneManager.LoadScene(currentSceneIndex - 1);
+                        //updateCurrentSceneIndex(false);
+
                     }
-                    SceneManager.LoadScene(currentSceneIndex);
+                    //SceneManager.LoadScene(currentSceneIndex);
                 }
 
             }
@@ -92,29 +90,30 @@ public class PageSwiping : MonoBehaviour
 
     }
 
-    //calculating distance between first touch and second touch
+    //calculating distance between two points on x,y coordinate plane
     private float calculateSwipeDistance()
     {
-        return Mathf.Sqrt(Mathf.Pow(Mathf.Abs(lastTouch.x - firstTouch.x), 2) + Mathf.Pow(Mathf.Abs(lastTouch.y - firstTouch.y), 2));
+        return Mathf.Sqrt(Mathf.Pow(Mathf.Abs(lastTouch.x - firstTouch.x), 2)
+            + Mathf.Pow(Mathf.Abs(lastTouch.y - firstTouch.y), 2));
     }
 
+    //only compare x value -- horizontal line
     private bool isSwipeRight()
     {
         return firstTouch.x < lastTouch.x;
     }
+
 
     //method for incrementing or decrementing scene index 
     private void updateCurrentSceneIndex(bool nextScreen)
     {
         if (nextScreen && (currentSceneIndex < totalSceneCount - 1))
         {
-            Debug.Log("current scene Index in if: " + currentSceneIndex);
             currentSceneIndex++;
 
         }
         else if (currentSceneIndex > 0)
         {
-            Debug.Log("current scene index in else: " + currentSceneIndex);
             currentSceneIndex--;
         }
     }
