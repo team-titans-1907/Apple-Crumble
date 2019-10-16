4e85c0c7637c2956de36b9c1fec93c7fed86b516
Lisa Jo <lisaxjo@gmail.com>
2019-10-15 22:27:32 -0400

    feat: validating  improved login fields

--- Assets/Scripts/Login.cs
+++ Assets/Scripts/Login.cs
@@ -4,13 +4,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
-
+using System.Text.RegularExpressions;
 
 public class Login : MonoBehaviour
 {
     public InputField nameField;
     public InputField passwordField;
     public Button submitButton;
+    public GameObject emailValidator;
+    public GameObject passwordValidator;
+    public const string MatchEmailPattern =
+     @"^(([\w-]+\.)+[\w-]+|([a-zA-Z]{1}|[\w-]{2,}))@"
+     + @"((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\."
+     + @"([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
+     + @"([a-zA-Z]+[\w-]+\.)+[a-zA-Z]{2,4})$";
+
+
 
     public void CallLogin()
     {
@@ -39,7 +48,17 @@ public class Login : MonoBehaviour
             Debug.Log("login fail. Error #");
         }
     }
-    public void VerifyInputs() { 
+    public void VerifyInputs() {
+        emailValidator.SetActive(false);
+        passwordValidator.SetActive(false);
+        if (passwordField.text.Length < 4)
+        {
+            passwordValidator.SetActive(true);
+        }
+        if (Regex.IsMatch(nameField.text, MatchEmailPattern) == false)
+        {
+            emailValidator.SetActive(true);
+        }
         submitButton.interactable = (nameField.text.Length >= 4 && passwordField.text.Length >= 4);
 
     }
