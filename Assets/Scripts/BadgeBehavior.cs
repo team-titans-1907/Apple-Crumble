8dc27d821fd6fd788d4f6fbef4c8aa6bce00038c
Kat Le <kl2853@barnard.edu>
2019-10-16 17:01:02 -0400

    chore: prototype badge box up and almost functional

---
+++ Assets/Scripts/BadgeBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BadgeBehavior : MonoBehaviour
+{
+    public GameObject panel;
+	private Scene activeScene;
+
+    void Start()
+	{
+		activeScene = SceneManager.GetActiveScene();
+		if (activeScene.name != "QuestComplete")
+		{
+            GameObject badge = GameObject.Find("Badge");
+            if (badge)
+            {
+                badge.SetActive(false);
+            }
+		}
+	}
+
+    public void OpenPanel()
+    {
+        if(panel != null)
+            {
+            bool isActive = panel.activeSelf;
+            panel.SetActive(!isActive);
+        }
+    }
+}
