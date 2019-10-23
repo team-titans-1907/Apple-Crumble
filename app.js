32723ef4346cce095816433463dcb1e154b7c5f2
Kat Le <kl2853@barnard.edu>
2019-10-23 13:40:53 -0400

    chore: removed unused node artifacts

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-
-app.get('/', (req, res) => {
-    res.send('Hello Unity Developers!');
-})
-app.listen(3000, () => console.log('started and listening.'));
